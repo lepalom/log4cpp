db970b2ddcde6ab5f694767a1836c7c6a12f11d8
Leopold Palomo <leopold.palomo@upc.edu>
2013-12-16 15:17:11 +0100

    MSThreads modifications

--- include/log4cpp/threading/MSThreads.hh
+++ include/log4cpp/threading/MSThreads.hh
@@ -45,7 +45,17 @@ namespace log4cpp {
          * identifiers look like is completely up to the underlying 
          * thread library.
          **/
-        std::string getThreadId();
+	
+	 LOG4CPP_EXPORT std::string getThreadId();
+        /**
+         * Return an identifier for the current thread. What these
+         * identifiers look like is completely up to the underlying
+         * thread library.
+                 *
+                 * @param buffer Character buffer of at least 16 in size
+                 * @return buffer
+         **/
+        LOG4CPP_EXPORT char* getThreadId(char* buffer);
         
         /**
          * A simple object wrapper around CreateMutex() and DeleteMutex()
